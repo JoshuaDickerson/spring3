e7c33910435620b9be08424600a9962538a35663
joshuadickerson <joshuajdickerson@gmail.com>
2013-12-14 19:00:13 -0500

    comments to help Mike

--- src/main/java/com/springapp/mvc/controller/PlayerController.java
+++ src/main/java/com/springapp/mvc/controller/PlayerController.java
@@ -10,27 +10,34 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class PlayerController {
     @Autowired
     PlayerDAO dao; // this is built and autowired
 
+    private Gson gson = new Gson();
+
 	@RequestMapping(method = RequestMethod.GET)
     @Transactional
 	public String printWelcome(ModelMap model) {
 
+       // Insert a new player into the database
+//        Player player = new Player();
+//        player.setLastName("dickerson");
+//        player.setFirstName("josh");
+//        player.setJersey(33);
+//        player.setBirthYear(1980);
+//        dao.insertPlayer(player);
 
-        Player player = new Player();
-        player.setLastName("dickerson");
-        player.setFirstName("josh");
-        player.setJersey(33);
-        player.setBirthYear(1980);
-        dao.insertPlayer(player);
+        // get all of the players
+        List<Player> players = dao.getPlayers();
 
-        Gson gson = new Gson();
-        String json = gson.toJson(player);
 
+        String json = gson.toJson(players.toArray());
+        // our model is like a dictionary we do lookups on in our jsp page
 		model.addAttribute("json", json);
         // here, json referrs to the json.jsp page being rendered
 		return "json";
--- src/main/java/com/springapp/mvc/persistence/PlayerDAO.java
+++ src/main/java/com/springapp/mvc/persistence/PlayerDAO.java
@@ -1,8 +1,11 @@
 package com.springapp.mvc.persistence;
 
 import com.springapp.mvc.model.Player;
+import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Required;
+
+import java.util.List;
 
 /**
  * Created by josh on 12/14/13.
@@ -13,6 +16,15 @@ public class PlayerDAO {
 
     public void insertPlayer(Player player){
         sessionFactory.getCurrentSession().save(player);
+    }
+
+    public Player getPlayerById(int playerId) {
+        return (Player) sessionFactory.getCurrentSession().get(Player.class, playerId);
+    }
+
+    public List<Player> getPlayers() {
+        Criteria criteria = sessionFactory.getCurrentSession().createCriteria(Player.class);
+        return criteria.list();
     }
 
 
